be0a4de3df23b6115f0269630d3a93ea9773d383
jkirira <jkirira@cytonn.com>
2022-01-29 07:03:18 +0300

    Changes for Heroku

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register')->middleware('guest');
