983271f0c9e7449bb81d09a3fa8bf91c2bfbdcce
jkirira <jkirira@cytonn.com>
2022-01-29 07:56:57 +0300

    Heroku Build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <title>ToDo</title>
 
