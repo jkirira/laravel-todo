f01b91a5b525df99dcd7ab14fccfc316e8cd4c4a
jkirira <jkirira@cytonn.com>
2022-02-07 10:28:49 +0300

    Auth Scaffolding

---
+++ app/native/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index() {
+        if(auth()->user()){
+            return redirect()->route('todos');
+        }
+        return view('login');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $authenticated = auth()->attempt([ 'email' =>$request->email, 'password' =>$request->password, ]);
+
+        if( !$authenticated ){
+            return back()->with('status', 'Invalid Login Details');
+        }
+
+        return redirect()->route('todos');
+
+    }
+
+}
